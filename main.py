0f29463728bccdeeeb8d2a9b8f64b5cb268ae072
luoxiaotian <luoxiaotian@bytedance.com>
2022-12-26 19:57:05 +0800

    feat: add other request

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import logging
 import requests
 from flask import Flask, jsonify
 from flask import request
+import json
 
 app = Flask(__name__)
 
@@ -30,6 +31,58 @@ def getUserInfo():
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
+@app.route("/juejin/article_get", methods=["POST"])
+def getArticles():
+    params = request.form.to_dict()
+    url = "https://api.juejin.cn/content_api/v1/article/query_list"
+    headers={
+        "Content-Type": "application/json; charset=utf-8",
+    }
+
+    req_body={
+        "cursor": params.get("cursor"),
+        "user_id": params.get("user_id"),
+        "sort_type": 2
+    }
+    res = requests.post(url, json=req_body, headers=headers)
+    response = jsonify(res.json())
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
+@app.route("/juejin/focus_get", methods=["GET"])
+def getFocus():
+    args = request.args
+    url = "https://api.juejin.cn/user_api/v1/follow/followees?user_id={}&cursor={}&sort_type=2".format(
+        args.get('user_id'), args.get('cursor')
+    )
+    headers={
+        "Content-Type": "application/json; charset=utf-8",
+    }
+
+    res = requests.get(url, headers=headers)
+    response = jsonify(res.json())
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route("/juejin/likes_get", methods=["POST"])
+def getLikes():
+    params = request.form.to_dict()
+    url = "https://api.juejin.cn/interact_api/v1/digg/query_page"
+    headers={
+        "Content-Type": "application/json; charset=utf-8",
+    }
+
+    req_body={
+        "cursor": params.get("cursor"),
+        "user_id": params.get("user_id"),
+        "sort_type": 2,
+        "item_type": 2
+    }
+    res = requests.post(url, json=req_body, headers=headers)
+    response = jsonify(res.json())
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 @app.route("/")
 def index():
@@ -37,4 +90,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
